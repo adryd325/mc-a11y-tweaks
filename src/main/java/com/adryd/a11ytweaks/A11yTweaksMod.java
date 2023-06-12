1e8920c1479a39e7bc681cab3ceed42da9562322
adryd <me@adryd.com>
2023-06-12 16:54:20 -0400

    1.20.1

--- src/main/java/com/adryd/a11ytweaks/A11yTweaksMod.java
+++ src/main/java/com/adryd/a11ytweaks/A11yTweaksMod.java
@@ -7,8 +7,6 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class A11yTweaksMod implements ModInitializer {
-	public static final Logger LOGGER = LoggerFactory.getLogger("a11ytweaks");
-
 	public static final A11yTweaksConfig CONFIG;
 
 	static {
